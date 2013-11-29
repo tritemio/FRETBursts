e9817f694241c9983d48106dfa7c87fb1cb8966e
Antonino Ingargiola <tritemio@gmail.com>
2013-11-29 15:24:52 -0800

    Software dir can be read from the environment.

--- notebooks/load_fretbursts.py
+++ notebooks/load_fretbursts.py
@@ -1,34 +1,47 @@
+#%%writefile load_fretbursts.py
 """
 Helper script to load the FretBurst software.
 
 This should be run from inside a notebook.
 """
 
-#%%writefile load_fretbursts.py
+# Modify these to point to your FretBursts folder
+# or set an environment variable FRETBURSTS_DIR containing the path
+# (the enviroment variable, if set, has the precedence).
+FRETBURSTS_DIR_WIN = r"C:\Data\Antonio\software\src\fretbursts"
+FRETBURSTS_DIR_LINUX = r"/home/user/src/fretbursts"
 
-from subprocess import check_output
+
 import os
-if os.name == 'posix':
-    BURST_DIR = r"/home/anto/Documents/ucla/src/burst/sources/fretbursts"
+from subprocess import check_output        
+from IPython.display import display, Math, clear_output
+from glob import glob                      # helps finding files
+
+
+if 'FRETBURSTS_DIR' in os.environ:
+    BURST_DIR = os.environ['FRETBURSTS_DIR']
+elif os.name == 'posix':
+    BURST_DIR = FRETBURSTS_DIR_LINUX
 elif os.name == 'nt':
-    BURST_DIR = r"C:\Data\Antonio\software\src\fretbursts"
+    BURST_DIR = FRETBURSTS_DIR_WIN
+else:
+    raise OSError ("Operating system not recognized (%s)." % os.name)
 
 ip = get_ipython()
+
+# Save current dir as NOTEBOOK_DIR
 if not 'NOTEBOOK_DIR' in globals():
     NOTEBOOK_DIR = ip.magic('%pwd')
-    
-from IPython.display import clear_output
-from glob import glob
 
-#ip.magic('%pylab inline')
 ip.magic('%matplotlib inline')
-ip.magic('%cd "$BURST_DIR"')
 
+ip.magic('%cd "$BURST_DIR"')
 ip.magic('%run -i burstlib.py')
 ip.magic('%run -i burst_plot.py')
 ip.magic('%run -i style')
 from utils.gui import gui_fname
 
+# If git is available, check fretbursts version
 if not git.git_path_valid():
     print('\nSoftware revision unknown (git not found).')
 else:
