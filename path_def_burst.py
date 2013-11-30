820d5b4f085fa2dcd815dfaf41af0a69de3eae75
Antonino Ingargiola <tritemio@gmail.com>
2013-11-29 19:48:27 -0800

    More general path definitions in path_def_burst.py

--- path_def_burst.py
+++ path_def_burst.py
@@ -1,23 +1,41 @@
 """
-This module defines paths for common folders
+In this module you can define common paths (data folder, git path, etc...).
+
+WARNING: Always use trailing slash "/" after a folder name.
 """
 
-# WARNING: Always use trailing slash "/" after a folder name.
+# DATA_DIR
+# Usually prepended to file names when loading data.
+# You can put this path in an environment variable FRETBURSTS_DATA_DIR
+# that, if defined, has the precedence on the variable set here
+_DATA_DIR_WIN = '../../../data/'
+_DATA_DIR_LINUX = '../data/'
+
+# GIT_PATH
+# Git is used to check the software revision during execution
+# Here you can set the full path to a git executable
+_GIT_PATH_WIN = (r'C:\Users\temp\AppData\Local\Atlassian\SourceTree/'
+                r'git_local\bin\git.exe')
+_GIT_PATH_LINUX = 'git'      # On *nix assumes that git is in the PATH
+
 
 import os
 
-
-# This allows different paths between linux and windows OS
 if os.name == 'posix':
     # Linux or Mac
-    data_dir = '../data/'
-    GIT_PATH = 'git'  # On *nix assumes that git is in the PATH
+    data_dir = _DATA_DIR_LINUX
+    GIT_PATH = _GIT_PATH_LINUX
 elif os.name == 'nt':
     # Windows
-    data_dir = '../../../data/'
-    GIT_PATH = r'C:\Users\temp\AppData\Local\Atlassian\SourceTree\git_local\bin\git.exe'
+    data_dir = _DATA_DIR_WIN
+    GIT_PATH = _GIT_PATH_WIN
 else:
     raise OSError ("Operating system not recognized (%s)." % os.name)
+
+# Overwrites data_dir if FRETBURSTS_DATA_DIR is defined
+if 'FRETBURSTS_DATA_DIR' in os.environ:
+    data_dir = os.environ['FRETBURSTS_DATA_DIR']
+
 
 alex_data_dir = data_dir+'/alex/'
 nsalex_data_dir = data_dir+'/nsAlex/'
