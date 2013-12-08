737e15d2f0daf863bd038fe2595b535384392235
Antonino Ingargiola <tritemio@gmail.com>
2013-12-07 22:42:01 -0800

    Slope fit uses euclidean distance add gamma support.

--- fret_fit.py
+++ fret_fit.py
@@ -31,7 +31,7 @@ def fit_E_binom(nd, na, noprint=False, method='c', **kwargs):
     """
     assert method in ['a', 'b', 'c']
     nd, na = np.round(nd).astype(int), np.round(na).astype(int)
-    
+
     # The binomial distribution can not handle negative values
     # so we must find a way to "remove" the negativa values
     # a. remove bursts with neg. values, but we can skew the fit
@@ -61,7 +61,7 @@ def fit_E_binom(nd, na, noprint=False, method='c', **kwargs):
 
         if not noprint: print " - Bursts left:", nd.size
         assert (nd>=0).all() and (na>=0).all()
-    min_kwargs = dict(bounds=(0,1), method='bounded', 
+    min_kwargs = dict(bounds=(0,1), method='bounded',
             options={'disp':1, 'xtol': 1e-6})
     min_kwargs.update(**kwargs)
     res = minimize_scalar(log_likelihood_binom, args=(nd,na), **min_kwargs)
@@ -102,10 +102,10 @@ def log_likelihood_poisson_nd(E, nd, na, bg_d):
 def fit_E_poisson_nt(nd, na, bg_a, **kwargs):
     """Fit the E using MLE with na extracted from a Poisson.
     """
-    min_kwargs = dict(bounds=(-0.1,1.1), method='bounded', 
+    min_kwargs = dict(bounds=(-0.1,1.1), method='bounded',
             options={'disp':1, 'xtol': 1e-6})
     min_kwargs.update(**kwargs)
-    res = minimize_scalar(log_likelihood_poisson_nt, args=(nd, na, bg_a), 
+    res = minimize_scalar(log_likelihood_poisson_nt, args=(nd, na, bg_a),
             **min_kwargs)
     E = res.x
     return E
@@ -113,24 +113,24 @@ def fit_E_poisson_nt(nd, na, bg_a, **kwargs):
 def fit_E_poisson_na(nd, na, bg_a, **kwargs):
     """Fit the E using MLE with na extracted from a Poisson.
     """
-    min_kwargs = dict(bounds=(-0.1,1.1), method='bounded', 
+    min_kwargs = dict(bounds=(-0.1,1.1), method='bounded',
             options={'disp':1, 'xtol': 1e-6})
     min_kwargs.update(**kwargs)
-    res = minimize_scalar(log_likelihood_poisson_na, args=(nd, na, bg_a), 
+    res = minimize_scalar(log_likelihood_poisson_na, args=(nd, na, bg_a),
             **min_kwargs)
     E = res.x
-    return E 
+    return E
 
 def fit_E_poisson_nd(nd, na, bg_d, **kwargs):
     """Fit the E using MLE with nd extracted from a Poisson.
     """
-    min_kwargs = dict(bounds=(-0.1,1.1), method='bounded', 
+    min_kwargs = dict(bounds=(-0.1,1.1), method='bounded',
             options={'disp':1, 'xtol': 1e-6})
     min_kwargs.update(**kwargs)
-    res = minimize_scalar(log_likelihood_poisson_nd, args=(nd, na, bg_d), 
+    res = minimize_scalar(log_likelihood_poisson_nd, args=(nd, na, bg_d),
             **min_kwargs)
     E = res.x
-    return E 
+    return E
 
 def fit_E_hist(nd, na, gamma=1., **kwargs):
     """Fit E using the histogram curve-fit (see gaussian_fit_hist).
@@ -141,7 +141,7 @@ def fit_E_hist(nd, na, gamma=1., **kwargs):
         w = get_weights(nd, na, weights=kwargs['weights'])
         kwargs.update(weights=w)
     E_mu, E_sig = gf.gaussian_fit_hist(E, mu0=0.5, sigma0=0.1, **kwargs)
-    return E_mu 
+    return E_mu
 
 def fit_E_cdf(nd, na, gamma=1., **kwargs):
     """Fit E using the CDF curve-fit (see gaussian_fit_cdf).
@@ -149,12 +149,19 @@ def fit_E_cdf(nd, na, gamma=1., **kwargs):
     """
     E = na/(na + gamma*nd)
     E_mu, E_sig = gf.gaussian_fit_cdf(E, mu0=0.5, sigma0=0.1, **kwargs)
-    return E_mu 
+    return E_mu
 
-def get_dist_euclid(nd, na, E_fit):
-    """Return the euclidean distance of (nd,na) from the fit line `E_fit`.
+def get_dist_euclid(nd, na, E_fit=None, slope=None):
+    """Returns the euclidean distance of (nd,na) from a fit line.
+    The fit line is specified by `slope` or by `E_fit`. Intercept is always 0.
     """
-    slope = E_fit/(1-E_fit)
+    if slope is None:
+        try:
+            slope = E_fit/(1-E_fit)
+        except TypeError:
+            # this happens if E_fit is None
+            raise ValueError('One among `E_fit` or `slope` must be not None.')
+
     # For each burst (nd,na) find the closest point on the fitted line
     nd_l = (slope*na + nd)/(slope**2+1)
     na_l = slope*nd_l
@@ -180,25 +187,33 @@ def get_weights(nd, na, weights, gamma=1.):
     assert weights.size == nd.size
     return weights
 
-def fit_E_slope(nd, na, weights=None):
+
+def fit_E_slope(nd, na, weights=None, gamma=1.):
     """Fit E with a least-squares fitting of slope on (nd,na) plane."""
-    weights = get_weights(nd,na,weights)
-    err_fun = lambda k, x, y, w: (y - k*x)*w
-    res = leastsq(err_fun, x0=1, args=(nd,na,weights))
+    weights = get_weights(nd, na, weights, gamma)
+    #err_fun = lambda k, x, y, w: (y - k*x)*w
+    err_fun = lambda k, x, y, w: w*get_dist_euclid(nd=x, na=y, slope=k)
+    res = leastsq(err_fun, x0=1, args=(nd, na, weights))
     #print res
     return res[0][0]/(res[0][0]+1)
 
-def fit_E_E_size(nd, na, weights=None):
+def fit_E_E_size(nd, na, weights=None, gamma=1., gamma_correct=False):
     """Fit the E with least-square minimization of errors on burst E values."""
-    weights = get_weights(nd,na,weights)
-    err_fun = lambda E_fit, d, a, w: (E_fit - 1.*a/(d+a))*w
-    res = leastsq(err_fun, x0=0.5, args=(nd,na,weights))
+    weights = get_weights(nd, na, weights, gamma)
+    if gamma_correct:
+        err_fun = lambda E_fit, d, a, w: (E_fit - 1.*a/(gamma*d+a))*w
+    else:
+        err_fun = lambda E_fit, d, a, w: (E_fit - 1.*a/(d+a))*w
+    res = leastsq(err_fun, x0=0.5, args=(nd, na, weights))
     return res[0][0]
 
-def fit_E_m(nd, na, weights=None):
+def fit_E_m(nd, na, weights=None, gamma=1., gamma_correct=False):
     """Fit the E with a weighted mean of burst E values."""
-    weights = get_weights(nd,na,weights)
-    E = na/(na+nd)
+    weights = get_weights(nd, na, weights, gamma)
+    if gamma_correct:
+        E = na/(na + gamma*nd)
+    else:
+        E = na/(na + nd)
     E_fit = np.dot(E, weights)/weights.sum()
     return E_fit
 
