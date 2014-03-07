7fc1e476e5282adad570cba61ac10906c92b5e7a
Antonino Ingargiola <tritemio@gmail.com>
2014-03-06 23:26:14 -0800

    fretbursts.py uses only import (no python run)

--- fretbursts.py
+++ fretbursts.py
@@ -4,12 +4,12 @@ Main module to run the fretbursts software.
 """
 
 from fretbursts_path_def import data_dir
-#from burstlib import *
-#from loaders import *
+from burstlib import *
+from loaders import *
 
-ip = get_ipython()
-ip.magic("run -i burstlib.py")
-ip.magic("run -i loaders.py")
+#ip = get_ipython()
+#ip.magic("run -i burstlib.py")
+#ip.magic("run -i loaders.py")
 
 #ip.magic("run -i burst_selection.py")
 #ip.magic("run -i burstsearch.py")
