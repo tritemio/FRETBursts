tritemio/FRETBursts

commit 35464f54ce61b65beb2799c8e521bd58e7559f7f

Add option for multi-ch burst-size threshold

select_bursts.py
@@ -79,6 +79,8 @@ def nda(d, ich=0, th1=20, th2=1000, gamma=1., gamma1=None,
         burst_size = (d.nd[ich] + 1.*d.na[ich]/gamma)
     if d.ALEX and add_naa:
         burst_size += d.naa[ich]
+    if np.size(th1) == d.nch: th1 = th1[ich]
+    if np.size(th2) == d.nch: th2 = th2[ich]
     bursts_mask = (burst_size >= th1)*(burst_size <= th2)
     s = "nda_th%d" % th1
     if th2 < 1000: s +="_th2_%d" % th2

commit f78f6a34e87fc24efaa3e1e867d3872284f74d70

Fix bug in select_bursts.nda with 1-ch data.
Bug introduced by 35464f5 commit (Feb. 21, 2014)

select_bursts.py
@@ -93,8 +93,8 @@ def nda(d, ich=0, th1=20, th2=1000, gamma=1., gamma1=None,
         burst_size = (d.nd[ich] + 1.*d.na[ich]/gamma)
     if d.ALEX and add_naa:
         burst_size += d.naa[ich]
-    if np.size(th1) == d.nch: th1 = th1[ich]
-    if np.size(th2) == d.nch: th2 = th2[ich]
+    if d.nch > 1 and (np.size(th1) == d.nch): th1 = th1[ich]
+    if d.nch > 1 and (np.size(th2) == d.nch): th2 = th2[ich]
     bursts_mask = (burst_size >= th1)*(burst_size <= th2)
     s = "nda_th%d" % th1
     if th2 < 1000: s +="_th2_%d" % th2