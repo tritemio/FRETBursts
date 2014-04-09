a15dc248562c760b0169210a747ea54cee99e54f
Antonino Ingargiola <tritemio@gmail.com>
2014-04-08 23:14:41 -0700

    Use FRETBursts name in load_fretbursts.py

--- notebooks/load_fretbursts.py
+++ notebooks/load_fretbursts.py
@@ -50,10 +50,10 @@ from utils import git
 
 # If git is available, check fretbursts version
 if not git.git_path_valid():
-    print('\nSoftware revision unknown (git not found).')
+    print('\nFRETBursts revision unknown (git not found).')
 else:
     last_commit = git.get_last_commit_line()
-    print('\nCurrent software revision:\n {}\n'.format(last_commit))
+    print('\nCurrent FRETBursts revision:\n {}\n'.format(last_commit))
     if not git.check_clean_status():
         print('\nWARNING -> Uncommitted changes:')
         print(git.get_status())
