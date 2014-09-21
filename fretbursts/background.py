4c5c8e118c1664d8385f97db665fb709166da59c
Antonino Ingargiola <tritemio@gmail.com>
2014-09-20 18:54:36 -0700

    Add function to fit BG as a function of Th for all the periods

--- fretbursts/background.py
+++ fretbursts/background.py
@@ -177,6 +177,42 @@ def fit_var_tail_us(d, Tail_min_us_list, t_max_s,
                 break # Skip remaining Tail_min
     return BG, BG_err
 
+def fit_varying_min_delta_ph(d, min_delta_ph_list, bg_fit_fun=exp_fit,
+                             ph_sel=Ph_sel('all'), **kwargs):
+    """
+    Fit the background as a function of the min photon interval threshold.
+
+    The background if fitted for all the channels, all the periods and all the
+    values in ``min_ph_iterval_list
+
+
+    Arguments:
+        d (Data object): the Data object containing the timestamps. You need to
+            call d.calc_bg() before calling this function in order to create
+            the background periods.
+        min_delta_ph_list (list or array): list of minimum photon separation
+            values above which photons are considered background. Unit: us.
+        bg_fit_fun (function): function used to fit the background.
+        ph_sel (Ph_sel object): photon selection on which the background is
+            computed. See :class:`fretbursts.ph_sel.Ph_sel` for details.
+    """
+
+    BG = np.zeros((d.nch, d.nperiods, np.size(min_delta_ph_list)))
+    BG_err = np.zeros_like(BG)
+    BG[:], BG_err[:] = None, None
+
+    for ich in range(d.nch):
+        for period, ph in enumerate(
+                              d.iter_ph_times_period(ich=ich, ph_sel=ph_sel)):
+            for i_min, min_delta_ph in enumerate(min_delta_ph_list):
+                try:
+                    BG[ich, period, i_min], BG_err[ich, period, i_min] = \
+                            bg_fit_fun(ph, tail_min_us=min_delta_ph,
+                                       clk_p=d.clk_p, **kwargs)
+                except AssertionError:
+                    break # Skip remaining Tail_min
+    return BG, BG_err
+
 ##
 # Other functions
 #
