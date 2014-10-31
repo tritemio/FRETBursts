tritemio/FRETBursts

commit 4781a70ee53963edd113e2ecf699bcf960c2d2dc

Add option to add `naa` to the weights

fretbursts/fret_fit.py
@@ -11,8 +11,7 @@ This module provides a standard interface for different fitting techniques.
 
 import numpy as np
 from scipy.stats import binom, expon
-from scipy.optimize import minimize_scalar, minimize, leastsq
-from utils.misc import pprint
+from scipy.optimize import minimize_scalar, leastsq
 
 import fit.gaussian_fitting as gf
 
@@ -173,14 +172,14 @@ def get_dist_euclid(nd, na, E_fit=None, slope=None):
     distances = np.sqrt((nd-nd_l)**2 + (na-na_l)**2)
     return distances
 
-def get_weights(nd, na, weights, gamma=1.):
+def get_weights(nd, na, weights, naa=0, gamma=1.):
     """Return burst weigths computed according to different criteria.
 
     `nd`, `na`: background corrected donor and acceptor ph. per burst
     `weights`: (string) type of weights (if None returns uniform weights).
                 see source for all the possible weights.
     """
-    nt = nd*gamma + na
+    nt = nd*gamma + na + naa
     if weights is None:           # all weights the same
         weights = np.ones(nd.size)
     elif weights is 'size':       # weight = burst size (with gamma)

fretbursts/burstlib_ext.py
@@ -27,7 +27,7 @@ import mfit
 
 
 def fit_bursts_kde_peak(dx, burst_data='E', bandwidth=0.03, weights='size',
-                        gamma=1, x_range=(-0.1, 1.1), x_ax=None,
+                        gamma=1, add_naa=False, x_range=(-0.1, 1.1), x_ax=None,
                         return_fitter=False):
     """Fit burst data (typ. E or S) by finding the KDE max on all the channels.
 
@@ -41,6 +41,7 @@ def fit_bursts_kde_peak(dx, burst_data='E', bandwidth=0.03, weights='size',
         weights (string or None): kind of burst-size weights.
             See :func:`fretbursts.fret_fit.get_weights`.
         gamma (float): gamma factor passed to `get_weights()`.
+        add_naa (bool): if True adds `naa` to the burst size.
         return_fitter (bool): if True returns the `MultiFitter` object
             used to fit the data. Default False.
 
@@ -49,16 +50,18 @@ def fit_bursts_kde_peak(dx, burst_data='E', bandwidth=0.03, weights='size',
         also return the `MultiFitter` object used to fit the data
     """
     fitter = calc_bursts_kde(dx, burst_data=burst_data, bandwidth=bandwidth,
-                             weights=weights, gamma=gamma)
+                             weights=weights, gamma=gamma, add_naa=add_naa)
     assert burst_data in dx
 
     if x_ax is None:
         x_ax = np.arange(-0.2, 1.2, 0.0002)
 
     fitter = mfit.MultiFitter(dx[burst_data])
+    weight_kwargs = dict(weights=weights, gamma=gamma, nd=dx.nd, na=dx.na)
+    if add_naa:
+        weight_kwargs.update(naa = dx.naa)
     fitter.set_weights_func(weight_func = fret_fit.get_weights,
-                            weight_kwargs = dict(weights=weights, gamma=gamma,
-                                             nd=dx.nd, na=dx.na))
+                            weight_kwargs = weight_kwargs)
     fitter.calc_kde(bandwidth=bandwidth)
     fitter.find_kde_max(x_ax, xmin=x_range[0], xmax=x_range[1])
     KDE_max_mch = fitter.kde_max_pos
@@ -69,7 +72,7 @@ def fit_bursts_kde_peak(dx, burst_data='E', bandwidth=0.03, weights='size',
         return KDE_max_mch
 
 def calc_bursts_kde(dx, burst_data='E', bandwidth=0.03,
-                    weights='size', gamma=1):
+                    weights='size', gamma=1, add_naa=False):
     """Compute KDE of burst data (typ. E or S) on all the channels.
 
     Parameters
@@ -88,7 +91,7 @@ def calc_bursts_kde(dx, burst_data='E', bandwidth=0.03,
     fitter = mfit.MultiFitter(dx[burst_data])
     fitter.set_weights_func(weight_func = fret_fit.get_weights,
                             weight_kwargs = dict(weights=weights, gamma=gamma,
-                                             nd=dx.nd, na=dx.na))
+                                                 nd=dx.nd, na=dx.na))
     fitter.calc_kde(bandwidth=bandwidth)
     return fitter
 

commit 193351c6b8458b4e9a1a9c2aff266c90000d376b

Fix regression in old fit methods introduced in 4781a70 (2014-08-28)

.gitignore
@@ -23,3 +23,4 @@ notebooks/dev/docs/docs_html
 notebooks/data
 *.lnk
 notebooks/dev/tests/*evaluated.ipynb
+.idea/

fretbursts/burstlib.py
@@ -2145,7 +2145,8 @@ class Data(DataContainer):
         fit_res, fit_model_F = zeros((self.nch, 2)), zeros(self.nch)
         for ich, (nd, na, E, mask) in enumerate(
                                         zip(self.nd, self.na, self.E, Mask)):
-            w = fret_fit.get_weights(nd[mask], na[mask], weights, gamma)
+            w = fret_fit.get_weights(nd[mask], na[mask],
+                                     weights=weights, gamma=gamma)
             # Compute weighted mean
             fit_res[ich, 0] = np.dot(w, E[mask])/w.sum()
             # Compute weighted variance
@@ -2214,7 +2215,7 @@ class Data(DataContainer):
         """
         fit_res = zeros((self.nch, 5))
         for ich, (nd, na, E) in enumerate(zip(self.nd, self.na, self.E)):
-            w = fret_fit.get_weights(nd, na, weights, gamma)
+            w = fret_fit.get_weights(nd, na, weights=weights, gamma=gamma)
             fit_res[ich, :] = fit_func(E, weights=w, **kwargs)
         self.add(fit_E_res=fit_res, fit_E_name=fit_func.__name__,
                 E_fit=fit_res[:,2], fit_E_curve=True,
@@ -2271,7 +2272,8 @@ class Data(DataContainer):
                 if weights is None:
                     w = None
                 else:
-                    w = fret_fit.get_weights(nd[mask], na[mask], weights, gamma)
+                    w = fret_fit.get_weights(nd[mask], na[mask],
+                                             weights=weights, gamma=gamma)
                 fit_res[ich, :] = fit_fun(E[mask], weights=w, **fit_kwargs)
             else:
                 # Non-histogram fits (PDF/CDF) do not support weights
@@ -2331,7 +2333,7 @@ class Data(DataContainer):
         for i, (Ech, nt, mask) in enumerate(zip(E_sel, self.nt, Mask)):
             nt_s = nt[mask]
             nd_s, na_s = self.nd[i][mask], self.na[i][mask]
-            w = fret_fit.get_weights(nd_s, na_s, weights)
+            w = fret_fit.get_weights(nd_s, na_s, weights=weights)
             info_ph = nt_s.sum()
             info_bu = nt_s.size
 

fretbursts/fret_fit.py
@@ -208,7 +208,7 @@ def get_weights(nd, na, weights, naa=0, gamma=1.):
 
 def fit_E_slope(nd, na, weights=None, gamma=1.):
     """Fit E with a least-squares fitting of slope on (nd,na) plane."""
-    weights = get_weights(nd, na, weights, gamma)
+    weights = get_weights(nd, na, weights=weights, gamma=gamma)
     #err_fun = lambda k, x, y, w: (y - k*x)*w
     err_fun = lambda k, x, y, w: w*get_dist_euclid(nd=x, na=y, slope=k)
     res = leastsq(err_fun, x0=1, args=(nd, na, weights))
@@ -217,7 +217,7 @@ def fit_E_slope(nd, na, weights=None, gamma=1.):
 
 def fit_E_E_size(nd, na, weights=None, gamma=1., gamma_correct=False):
     """Fit the E with least-square minimization of errors on burst E values."""
-    weights = get_weights(nd, na, weights, gamma)
+    weights = get_weights(nd, na, weights=weights, gamma=gamma)
     if gamma_correct:
         err_fun = lambda E_fit, d, a, w: (E_fit - 1.*a/(gamma*d+a))*w
     else:
@@ -227,7 +227,7 @@ def fit_E_E_size(nd, na, weights=None, gamma=1., gamma_correct=False):
 
 def fit_E_m(nd, na, weights=None, gamma=1., gamma_correct=False):
     """Fit the E with a weighted mean of burst E values."""
-    weights = get_weights(nd, na, weights, gamma)
+    weights = get_weights(nd, na, weights=weights, gamma=gamma)
     if gamma_correct:
         E = na/(na + gamma*nd)
     else: