87b351873f72e9e7534b1b18a0fa9fc4507fcd40
Antonino Ingargiola <tritemio@gmail.com>
2014-11-16 16:03:26 -0800

    More robust implementation of single-ch shortcut

--- fretbursts/burstlib.py
+++ fretbursts/burstlib.py
@@ -718,26 +718,29 @@ class Data(DataContainer):
         DataContainer.__init__(self, **init_kw)
 
     ## Single-spot shortcuts
-    def __getattr__(self, field):
+    def __getattr__(self, name):
         """Single-channel shortcuts for per-channel fields.
 
         Appending a '_' to a per-channel field avoids specifying the channel.
         For example use d.nd_ instead if d.nd[0].
         """
-        if not field.endswith('_'):
-            raise AttributeError("Data object has no attribute '%s'" % field)
-        name = field[:-1]
+        msg_missing_attr = "'%s' object has no attribute '%s'" % \
+                                            (self.__class__.__name__, name)
+        if name.startswith('_') or not name.endswith('_'):
+            raise AttributeError(msg_missing_attr)
+
+        field = name[:-1]
         try:
-            value = self.__getitem__(name)
+            value = self.__getitem__(field)
         except KeyError:
-            raise AttributeError("Data object has no attribute '%s'" % name)
+            raise AttributeError(msg_missing_attr)
         else:
             # Support lists, tuples and object with arrays interface
             # (i.e. numpy arrays or pandas objects).
             if type(value) in [list, tuple] or hasattr(value, '__array__'):
                 if len(value) == self.nch:
                     return value[0]
-            raise ValueError('Name "%s" is not a per-channel field.' % name)
+            raise ValueError('Name "%s" is not a per-channel field.' % field)
 
 
     def copy(self, mute=False):
