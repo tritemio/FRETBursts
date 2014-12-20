30cf2a166d6ffe93c5df2443ee104f302c19040d
Antonino Ingargiola <tritemio@gmail.com>
2014-12-19 17:40:24 -0800

    Update SM file reader function to decode the header

--- fretbursts/dataload/smreader.py
+++ fretbursts/dataload/smreader.py
@@ -9,7 +9,7 @@ SM Format, written by the LV program in WeissLab us-ALEX setup
 
 A SM file is composed by two parts:
 
-  - 166 bytes: an header
+  - XXX bytes: an header (usually 166 bytes)
   - the remainig bytes: the data
   - 26 bytes of trailing cruft
 
@@ -30,17 +30,103 @@ are the detector.
 
 The data is in unsigned big endian (>) format.
 
-For efficiency the data is read as a list of B.E. (>) uint32 and then
-recomposed in ph_times and det. (NB: Old not very efficient approach)
-
 The proper way to read the data is to read the byte-stream and interpret
 it as a record array in which each element is 12 bytes.
 """
 
 import numpy as np
 
+class Decoder:
+    def __init__(self, buffer):
+        self.buff = buffer
+        self.cursor = 0
 
-def load_sm(fname, header=166):
+    def readscalar(self, size=4, basetype='u', endiness='>', inplace=False):
+        dtype = endiness + basetype + str(size)
+        buffer = self.buff[self.cursor:self.cursor + size]
+        if not inplace:
+            self.cursor += size
+        return np.frombuffer(buffer=buffer, dtype=dtype)
+
+    def readstring(self, size_max=256, inplace=False, **kwargs):
+        orig_cursor = self.cursor
+        size = int(self.readscalar(**kwargs))
+        if size > size_max:
+            print('Big string: size limited to %d.' % size_max)
+            size = size_max
+
+        string = self.buff[self.cursor:self.cursor + size]
+        self.cursor = orig_cursor if inplace else (self.cursor + size)
+        return string
+
+def decode_header(data):
+    """
+    Decode the cervellotic header of the SM file.
+
+    Returns
+        A 2-element tuple with header-size and a list of channel labels
+    """
+    decoder = Decoder(data)
+    version = decoder.readscalar()
+    string1 = decoder.readstring()   # -> Comment
+    string2 = decoder.readstring()   # -> 'Simple'
+    pointer1 = decoder.readscalar()  # -> Pointer to 8 bytes before the end
+    string3 = decoder.readstring()   # -> File section type
+
+    magic1 = decoder.readscalar()
+    magic2 = decoder.readscalar()
+
+    col1_name = decoder.readstring()
+    col1_resolution = decoder.readscalar(size=8, basetype='f')
+    col1_offset = decoder.readscalar(size=8, basetype='f')
+    col1_bho = decoder.readscalar()
+
+    col2_name = decoder.readstring()
+    col2_resolution = decoder.readscalar(size=8, basetype='f')
+    col2_offset = decoder.readscalar(size=8, basetype='f')
+    col2_bho = decoder.readscalar()
+
+    col3_name = decoder.readstring()
+    col3_resolution = decoder.readscalar(size=8, basetype='f')
+    col3_offset = decoder.readscalar(size=8, basetype='f')
+    num_channels = decoder.readscalar()
+
+    ch_labels = [decoder.readstring() for _ in range(num_channels)]
+
+    return decoder.cursor, ch_labels
+
+
+def load_sm(fname, return_labels=False):
+    """Read an SM data file.
+
+    Return
+        timestamps, detectors and optionally a list of detectors labels
+    """
+    with open(fname, 'rb') as f:
+        fulldata = f.read()
+
+    header_size, labels = decode_header(fulldata)
+    rawdata = fulldata[header_size:]
+
+    # Remove the end of the file
+    end_field1 = 4
+    end_str = 'End Of Run'
+    end_field2 = 12
+    valid_size = len(rawdata) - end_field1 - len(end_str) - end_field2
+
+    # Description of the record element in the file
+    sm_dtype = np.dtype([('timestamp', '>i8'), ('detector', '>u4')])
+
+    # View of the binary data as an array (no copy performed)
+    data = np.frombuffer(rawdata[:valid_size], dtype=sm_dtype)
+
+    if return_labels:
+        return data['timestamp'], data['detector'], labels
+    else:
+        return data['timestamp'], data['detector']
+
+
+def load_sm_legacy(fname, header=166):
     try: f = open(fname, 'rb')
     except IOError: f = open(fname+'.sm', 'rb')
     head = f.read(header)
@@ -53,56 +139,4 @@ def load_sm(fname, header=166):
                 data[:,1].astype('int64')
     det = data[:,2].astype('uint8') # 8 bit are enough (also bool!)
     return ph_times, det
-
-def load_sm_new(fname, header=166):
-    try: f = open(fname, 'rb')
-    except IOError: f = open(fname+'.sm', 'rb')
-    f.seek(header)
-    byte_string = f.read()
-
-    # Description of the record element in the file
-    sm_dtype = np.dtype([('timestamp', '>i8'), ('detector', '>u4')])
-
-    # Remove the end of the file
-    end_field1 = 4
-    end_str = 'End Of Run'
-    end_field2 = 12
-    valid_size = len(byte_string) - end_field1 - len(end_str) - end_field2
-
-    # View of the binary data as an array (no copy performed)
-    data = np.frombuffer(byte_string[:valid_size], dtype=sm_dtype)
-    return data['timestamp'], data['detector']
-
-def _decode_header(header):
-    """Decode the header of a .sm file. UNUSED."""
-    # List of ASCII strings in the header
-    str_list = [
-            'Simple', 'Arrival Time Counter',
-            'Time High', 'Time Low',
-            'Ch 1', 'Ch 2'
-            ]
-
-    # List of ASCII byte fields lengths
-    field_list = [12, 8, 12, 24, 55, 4]
-
-    str_len = len(''.join(str_list))
-    field_len = reduce(lambda x,y: x+y, field_list)
-
-    assert len(str_list) == len(field_list)
-    assert str_len + field_len == len(header)
-
-    Fields = []
-    Strings = []
-    pos = 0
-    for field, string in zip(field_list, str_list):
-        # Read the sequence of byte fields and strings
-        d = np.frombuffer(header[pos:pos+field], count=field/4, dtype='>u4')
-        pos += field
-        s = header[pos:pos+len(string)]
-        pos += len(string)
-        Fields.append(d)
-        Strings.append(s)
-
-    return Strings, Fields
-
 
