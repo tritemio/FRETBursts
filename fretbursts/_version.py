970a57f039459343e2ed7ea902ec36dc87dbd2a7
Antonino Ingargiola <tritemio@gmail.com>
2015-11-06 18:40:34 -0800

    Fix versioneer

--- fretbursts/_version.py
+++ fretbursts/_version.py
@@ -36,7 +36,7 @@ def get_config():
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = """"
+    cfg.tag_prefix = ""
     cfg.parentdir_prefix = "fretbursts-"
     cfg.versionfile_source = "fretbursts/_version.py"
     cfg.verbose = False
