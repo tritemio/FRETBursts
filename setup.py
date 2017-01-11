634f1d883a43eda5907c64204f3a704da25670c4
Antonino Ingargiola <tritemio@gmail.com>
2017-01-11 09:43:55 -0800

    DOC: fix typo

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ FRETBursts
 single-molecule FRET (smFRET) measurements. It can analyze both single-spot
 and multi-spot smFRET data with or without alternating laser excitation (ALEX).
 
-From more info please refer to:
+For more info please refer to:
 
 - **FRETBursts: An Open Source Toolkit for Analysis of Freely-Diffusing Single-Molecule FRET**
   *Ingargiola et. al.* (2016). PLoS ONE doi: `10.1371/journal.pone.0160716 <10.1371/journal.pone.0160716>`__.
